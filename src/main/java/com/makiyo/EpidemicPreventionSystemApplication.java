b59cc9cf92a7992990c7b496a91d1c0e0dffa4ca
Makiyo <lin171820@gmail.com>
2022-06-08 14:59:20 +0800

    Mybatis和logging日志信息配置

--- src/main/java/com/makiyo/EpidemicPreventionSystemApplication.java
+++ src/main/java/com/makiyo/EpidemicPreventionSystemApplication.java
@@ -1,4 +1,4 @@
-package com.makiyo.epidemicpreventionsystem;
+package com.makiyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
