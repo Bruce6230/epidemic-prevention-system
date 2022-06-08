d212a27ba73e0bf1f4a630c2b55c6523b2a12674
Makiyo <lin171820@gmail.com>
2022-06-08 14:39:32 +0800

    项目初始化

---
+++ src/main/java/com/makiyo/epidemicpreventionsystem/EpidemicPreventionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.makiyo.epidemicpreventionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicPreventionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicPreventionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makiyo/epidemicpreventionsystem/EpidemicPreventionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makiyo.epidemicpreventionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicPreventionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
