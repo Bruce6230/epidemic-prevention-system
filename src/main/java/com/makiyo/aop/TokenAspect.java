deab66b7b6b69669d3a085472c37816ed053dbcd
Makiyo <lin171820@gmail.com>
2022-06-10 09:14:45 +0800

    创建TokenAspect切面类拦截Control所有请求与响应

---
+++ src/main/java/com/makiyo/aop/TokenAspect.java
@@ -0,0 +1,34 @@
+package com.makiyo.aop;
+
+import com.makiyo.config.ThreadLocalToken;
+import com.makiyo.utils.Response;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TokenAspect {
+    @Autowired
+    private ThreadLocalToken threadLocalToken;
+
+    @Pointcut("execution(public * com.makiyo.controller.*.*(..)))")
+    public void aspect()
+    {
+
+    }
+    @Around("aspect()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        Response response = (Response) point.proceed();
+        String token = threadLocalToken.getToken();
+        if(token!=null)
+        {
+            response.put("token",token);
+            threadLocalToken.clear();
+        }
+        return response;
+    }
+}
