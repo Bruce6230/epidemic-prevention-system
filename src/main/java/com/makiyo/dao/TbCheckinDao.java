52f8feb98141ce4e87e04a56af7452fb920cba20
makiyo <lin171820@gmail.com>
2022-06-23 18:41:46 +0800

    签到条件检测（持久层）

--- src/main/java/com/makiyo/dao/TbCheckinDao.java
+++ src/main/java/com/makiyo/dao/TbCheckinDao.java
@@ -3,6 +3,8 @@ package com.makiyo.dao;
 import com.makiyo.pojo.TbCheckin;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
 @Mapper
 public interface TbCheckinDao {
     int deleteByPrimaryKey(Integer id);
@@ -16,4 +18,6 @@ public interface TbCheckinDao {
     int updateByPrimaryKeySelective(TbCheckin record);
 
     int updateByPrimaryKey(TbCheckin record);
+
+    public Integer havaCheckin(HashMap param);
 }
