9b91db3d79590476b63030e612f0ad8ca1572d8d
Makiyo <lin171820@gmail.com>
2022-07-13 01:29:34 +0800

    SMTP发送邮件

---
+++ src/main/java/com/makiyo/email/EmailTask.java
@@ -0,0 +1,27 @@
+package com.makiyo.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@Scope("prototype")
+public class EmailTask implements Serializable {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Value("${eps.email.system}")
+    private String mailbox;
+
+    @Async
+    public void sendAsync(SimpleMailMessage message){
+        message.setFrom(mailbox);
+        message.setCc(mailbox);
+        javaMailSender.send(message);
+    }
+}
